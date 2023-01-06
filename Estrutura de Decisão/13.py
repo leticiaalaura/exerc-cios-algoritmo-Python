6a9954a456fa61f6fa194ff182db893996f52910
Letícia <leticiaalaura@gmail.com>
2023-01-06 15:53:16 -0300

    13 - Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.

---
+++ Estrutura de Decisão/13.py
@@ -0,0 +1,18 @@
+n = int(input('Digite um numero entre 1 e 7: '))
+
+if n == 1:
+  print('Domingo')
+elif n == 2:
+  print('Segunda-feira')
+elif n == 3:
+  print('Terça-feira')
+elif n == 4:
+  print('Quarta-feira')
+elif n == 5:
+  print('Quinta-feira')
+elif n == 6:
+  print('Sexta-feira')
+elif n == 7:
+  print('Sábado')
+else:
+  print('valor inválido.')
